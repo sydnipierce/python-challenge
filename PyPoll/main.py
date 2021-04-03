95dc00aec9f092058702640f3a5de536ce9baf7b
Sydni <sydnimpierce@gmail.com>
2021-04-03 14:35:36 -0400

    Solved writer read-only error, created correct text write code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,11 @@ import os
 import csv
 import pandas as pd
 
+khan_count = 0
+li_count = 0
+correy_count = 0
+otooley_count = 0
+
 'Create file path for election_data.csv file'
 election_data_file_path = os.path.join('Resources', 'election_data.csv')
 
@@ -27,17 +32,51 @@ with open(election_data_file_path) as election_csvfile:
     for row in csvreader:
         voters.append(row[0])
         candidates.append(row[2])
+
+        if row[2] == "Khan":
+            khan_count = khan_count + 1
+
+    for x in range(5):
+        print(voters[x])
+        print(candidates[x])
         
     'Pull a list of unique candidates'
     candidates_set = set(candidates)
     unique_candidates = list(candidates_set)
     print(unique_candidates)
 
-    khan_count = 0
-    li_count = 0
-    correy_count = 0
-    otooley_count = 0
+    total_votes = len(voters)
+    print(total_votes)
 
     for row in csvreader:
-        if row[2] == candidates_set[0]:
-            khan_count = khan_count 
+        if row[2] == "Khan":
+            khan_count = khan_count + 1
+        elif row[2] == "Li":
+            li_count += 1
+        elif row[2] == "Correy":
+            correy_count += 1
+        elif candidate == "O'Tooley":
+            otooley_count += 1
+    
+        print(candidate)
+
+    khan_percent = khan_count / total_votes
+    li_percent = li_count / total_votes
+    correy_percent = correy_count / total_votes
+    otooley_percent = otooley_count / total_votes
+
+    print(khan_count)
+    print(li_count)
+    print(correy_count)
+    print(otooley_count)
+
+    if (khan_count > li_count) and (khan_count > correy_count) and (khan_count > otooley_count):
+        winner = "Khan"
+    elif (li_count > khan_count) and (li_count > correy_count) and (li_count > otooley_count):
+        winner = "Li"
+    elif (correy_count > khan_count) and (correy_count > li_count) and (correy_count > otooley_count):
+        winner = "Correy"
+    elif (otooley_count > khan_count) and (otooley_count > li_count) and (otooley_count > correy_count):
+        winner = "O'Tooley"
+    else:
+        winner = "None"
