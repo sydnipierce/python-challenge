442208654c755961331dbeb377d657f719233d62
Sydni <sydnimpierce@gmail.com>
2021-04-02 23:30:57 -0400

    Bank progress 3

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,16 +18,32 @@ with open(budget_data_file_path) as budget_csvfile:
     'Create empty lists to collect .csv column data'
     month = []
     profit_or_loss = []
+    profit_change = []
 
-    'Find the number of records in the table'
+    'Read header row, put cursor on next data line'
     header = next(csvreader)
-    total_months = 0
-    for row in csvreader:
-        total_months = total_months + 1
-        month.append(row[0])
-        profit_or_loss.append(row[1])
 
+    'Loop through data rows and create lists from columns'
+    for row in csvreader:
+        month.append(row[0])
+        profit_or_loss.append(int(row[1]))
+        
+    'Calculate the change in profit between each month and create a list'
+    for x in range(1, 87) in profit_or_loss:
+        monthly_change = profit_or_loss[x] - profit_or_loss[x - 1]
+        profit_change.append(monthly_change)
+
+    total_months = len(month)
     net_profit = sum(profit_or_loss)
+    avg_profit_change = mean(profit_change)
+    max_profit = max(profit_change)
+    max_loss = min(profit_change)
+
+    
+
+    for profit in profit_change:
+        if profit == max_profit:
+
 
     print(total_months)
-
+    print(net_profit)
